fd93f8abe9c5e44d8210e1e6ba2244752c47900c
Theomariee <theo.marie5@gmail.com>
2018-02-12 16:57:56 +0100

    script.sh2

--- script.sh
+++ script.sh
@@ -7,4 +7,5 @@ filename=$output_head$random$output_tail
 
 $(curl -u f4d7c70e-074a-4866-afae-f63e4116e3d5: https://api.sncf.com/v1/coverage/sncf/stop_areas/stop_area:OCE:SA:87447003/departures > $filename)
 
+echo "fichier json crée : "$filename
 
