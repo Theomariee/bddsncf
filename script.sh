68a8fc738bb352b9aa639a24aa0470943a85ad6a
Theomariee <theo.marie5@gmail.com>
2018-02-12 17:39:28 +0100

    Added first version of script.sh - Outputs .json file containing result of query returned by the API

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+#check gares.io pour doc
+random=$(pwgen 20 1)
+output_head="output_"
+output_tail=".json"
+filename=$output_head$random$output_tail
+
+$(curl -u f4d7c70e-074a-4866-afae-f63e4116e3d5: https://api.sncf.com/v1/coverage/sncf/stop_areas/stop_area:OCE:SA:87447003/departures > $filename)
+
+echo "fichier json crée : "$filename
+
