d54faf737a31683cdfa2fbe95a989feb05e21bcf
Theomariee <theo.marie5@gmail.com>
2018-04-10 20:57:57 +0200

    maj

--- createGares.py
+++ createGares.py
@@ -1,66 +1,61 @@
 #!/usr/bin/python3.4
 import psycopg2, json 
+
 from config import config 
 from prepare import PreparingCursor
 
 def createGares():
     """ Create and fill "gares" table to the PostgreSQL database server """
     conn = None
-    try:
-        # read connection parameters
-        params = config()
+
+    # read connection parameters
+    params = config()
  
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params)
- 
-        # create a cursor
-        cur = conn.cursor()
+    # connect to the PostgreSQL server
+    print('Connecting to the PostgreSQL database...')
+    #conn = psycopg2.connect(**params)
+    conn = psycopg2.connect("dbname='sncf' user='postgres' host='localhost' password='esaco15obofo'") 
 
-        cur.execute("""CREATE TABLE IF NOT EXISTS gares (
-	uic CHAR(10) PRIMARY KEY NOT NULL,
-	departement CHAR(100) NOT NULL,
-	intitule CHAR(200) NOT NULL,
-	region CHAR(100) NOT NULL,
-	commune CHAR(200) NOT NULL,
-	latitude NUMERIC(10,7) NOT NULL,
-	longitude NUMERIC(10,7) NOT NULL)""")
-        cur.close()
-	
-
-        cur = conn.cursor(cursor_factory=PreparingCursor)
-        
-        #execute statement
-        cur.prepare("INSERT INTO gares (uic, departement, intitule, region, commune, latitude, longitude) VALUES (%s,%s,%s,%s,%s,%s,%s)")
-        with open('output/outputgares.json', 'r') as f:
-            data = json.load(f)
-	    print(data)
-	    type(data)
-        for rec in data :
-            uic = rec.get("uic")
-            departement = rec.get("departement")
-            intitule = rec.get("intitule_gare")
-            region = rec.get("region")
-            commune = rec.get("commune")
-            latitude = rec.get("latitude_wgs84")
-            longitude = rec.get("longitude_wgs84")
-            print(uic, departement, intitule, region, commune, latitude, longitude)
-            #cur.execute((uic, departement, intitule, region, commune, latitude, longitude))
-
-        
-
-        
+    # create a cursor
+    cur = conn.cursor()
+    cur.execute("""DROP TABLE gares;""")
+    cur.execute("""CREATE TABLE IF NOT EXISTS gares (uic VARCHAR(10) PRIMARY KEY NOT NULL, departement VARCHAR(100) NOT NULL, intitule VARCHAR(200) NOT NULL, region VARCHAR(100) NOT NULL, commune VARCHAR(200) NOT NULL, latitude FLOAT(10) NOT NULL, longitude FLOAT(10) NOT NULL);""")
+    cur.close()
+    print(conn.notices)
+    conn.commit()
+    conn.close()       
+    #execute statement
+    with open('output/outputgares.json', 'r') as f:
+        data = json.loads(f.read())   	   
+    for rec in data: 
+	conn = psycopg2.connect("dbname='sncf' user='postgres' host='localhost' password='esaco15obofo'")
+	cur = conn.cursor()
+	#print(rec)
+	uic = rec.get("uic")
+        departement = rec.get("departement")
+        intitule = rec.get("intitule_gare")
+        region = rec.get("region")
+        commune = rec.get("commune")
+        latitude = rec.get("latitude_wgs84")
+        longitude = rec.get("longitude_wgs84")
+        print("Insertion : gare(", uic, departement, intitule, region, commune, latitude, longitude, ")...")
+	try:
+            cur.execute("""INSERT INTO gares (uic, departement, intitule, region, commune, latitude, longitude) VALUES (%s,%s,%s,%s,%s,%s,%s);""", (uic, departement, intitule, region, commune, latitude, longitude))
+	    cur.close()
+	    conn.commit()
+	    conn.close()
+	except (Exception, psycopg2.DatabaseError) as error:
+	    pass
+	    #print(error)	
 
         
  # execute a statement
         
        
      # close the communication with the PostgreSQL
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
+
+      
+    if conn is not None:
             conn.close()
             print('Database connection closed.')
  
